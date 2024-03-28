a63284f5a51e3090aa736a70c80716c4260ec150
wesleyara <wes1gom@gmail.com>
2024-03-28 10:51:54 -0300

    feat: adding methods in script

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-console.log("hello world")
+import { AtomicService } from "./services/AtomicService";
+import { QuestionService } from "./services/QuestionService";
+import { RPCService } from "./services/RPCService";
+
+(async () => {
+  const atomicService = new AtomicService();
+  const rpcService = new RPCService();
+  const questionService = new QuestionService();
+
+  const { type, endpoint } = await questionService.make();
+
+  if (type === "RPC") {
+    return await rpcService.test(endpoint.trim());
+  }
+
+  return atomicService.test(endpoint.trim());
+})();
